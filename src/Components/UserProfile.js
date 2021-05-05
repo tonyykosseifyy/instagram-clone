8e2153fc8be6f1c70c0f6b82e9e62c950baa840f
Tony Kosseify <tonykosseify123@gmail.com>
2021-05-05 13:14:53 +0300

    Post Container made responsive

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -14,7 +14,15 @@ import UserProfileSkeleton from './UserProfileSkeleton' ;
 import PostSkeleton from './PostUserSkeleton' ;
 import { notification } from 'antd' ;
 import 'antd/dist/antd.css' ;
+import styled from 'styled-components' ;
 
+const postContainerFunction = (length) => {
+    if( (length / 3) > Math.floor(length / 3)) {
+        return Math.floor(length / 3) + 1 ;
+    } else {
+        return Math.floor(length / 3)
+    }
+}
 
 const UserProfile = () => {
     const dark = useSelector(state => state.darkTheme) ;
@@ -24,16 +32,16 @@ const UserProfile = () => {
     const [ posts , setPosts ] = useState([]) ;
     const [ postsError , setPostsError ] = useState(false) ;
     const [ postsComments , setPostsComments ] = useState([]) ;
-    const [ postFocus , setPostFocus ] = useState() ; 
+    const [ postFocus , setPostFocus ] = useState() ;
     const [ chosenIndex , setChosenIndex ] = useState() ;
     const [ loading , setLoading ] = useState(true) ;
     const [ loadingPosts , setLoadingPosts ] = useState(true) ;
-
+    const [ postContainer , setPostContainer ] = useState([]) ;
     useEffect(() => {
         const args = {
             message: 'New notification' ,
             description: 'Feel free to click on a post to add a comment' ,
-            duration: 4.5 
+            duration: 4.5
         }
         notification.open(args)
     }, [])
@@ -72,19 +80,29 @@ const UserProfile = () => {
                     ])
                 })
             })
-    } 
-    }, [posts] )
-    const postContainer = (length) => {
-        if( (length / 3) > Math.floor(length / 3)) {
-            return Math.floor(length / 3) + 1 ;
-        } else {
-            return Math.floor(length / 3)
-        }
     }
+    }, [posts] )
+
+    useEffect(() => {
+      if (posts.length > 0 ) {
+        const num = postContainerFunction(posts.length) ;
+        let first = 0 ;
+        let postscontainer = [] ;
+        for (let i = 0 ; i < num ; i++ ) {
+          postscontainer.push(posts.slice(first , first + 3 )) ;
+          first+= 3 ;
+          console.log('post container ==> ' , postscontainer)
+        }
+        console.log(num , first , postContainer)
+        setPostContainer(postscontainer)
+      }
+    },[ posts ])
+    console.log(postContainer
+       , 'testedd')
     if ( !error ) {
-        return ( 
+        return (
             <div className='user-profile' style={{ background : dark ?  '#171717' : 'inherit' , color: dark ? 'white' : 'inherit'}}>
-                { user && !loading ? 
+                { user && !loading ?
 
                 <div className='user-profile-top'>
                     <PostProfile newUser={newUser} postFocus={postFocus} setPostFocus={setPostFocus} comments={postsComments[chosenIndex]} />
@@ -108,17 +126,17 @@ const UserProfile = () => {
                             </ul>
                         </div>
                     </div>
-                    
-                </div> 
-                
-                : 
+
+                </div>
+
+                :
                 <UserProfileSkeleton />
                  }
                 { (!postsError && posts.length > 0 && !loadingPosts ) ? (
-                    
+
                     <div className='user-profile-posts-section'>
                         <div className='user-profile-bar'></div>
-                        <div className='user-profile-posts' style={{gridTemplateRows: `repeat(${postContainer(posts.length)} , 270px)`}}>
+                        <PostContainerGrid postsLength={posts.length} >
                             {posts.map((item , index) => (
                                 <div className='post-container' onClick={() => onPostClick(item , index)}>
                                     <img src={item.image} key={index} alt={item.text} />
@@ -126,19 +144,19 @@ const UserProfile = () => {
                                         <div style={{color: 'white'}}>
                                             <FavoriteIcon />
                                             <strong style={{marginLeft: '6px' , marginRight: '15px'}}>{item.likes}</strong>
-                                            {postsComments.length == posts.length && 
+                                            {postsComments.length == posts.length &&
                                             <>
                                             <FaComment />
                                             <strong  style={{marginLeft: '6px'}}>
                                                 { postsComments[index].length }
-                                            </strong> 
+                                            </strong>
                                             </>
                                             }
                                         </div>
                                     </div>
                                 </div>
                             ))}
-                        </div>
+                        </PostContainerGrid>
                     </div>
                 ) : <PostSkeleton /> }
             </div>
@@ -152,4 +170,31 @@ const UserProfile = () => {
         ) ;
 }};
 
-export default UserProfile 
+export default UserProfile ;
+
+
+
+const PostContainerGrid = styled.div`
+  display: grid ;
+  grid-template-columns: repeat(3 , 1fr) ;
+  grid-template-rows: ${(props) => `repeat(${postContainerFunction(props.postsLength)} , 220px)`};
+  grid-column-gap: 20px ;
+  grid-row-gap: 20px ;
+  max-width: 950px ;
+  @media (max-width: 1124px) {
+    grid-template-rows: ${(props) => `repeat(${postContainerFunction(props.postsLength)} , 190px)`};
+    grid-column-gap: 15px ;
+    grid-row-gap: 15px ;
+  }
+  @media (max-width: 800px) {
+    grid-template-rows: ${(props) => `repeat(${postContainerFunction(props.postsLength)} , 150px)`};
+    grid-column-gap: 6px ;
+    grid-row-gap: 6px ;
+    padding-bottom: 70px ;
+  }
+  @media (max-width: 500px ) {
+    grid-template-rows: ${(props) => `repeat(${postContainerFunction(props.postsLength)} , 100px)`};
+    grid-column-gap: 3px ;
+    grid-row-gap: 3px ;
+  }
+`
