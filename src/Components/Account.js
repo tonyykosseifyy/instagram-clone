9c6f360cf59c6a8c7bcc292c6c0a56d987c5773d
Tony Kosseify <tonykosseify123@gmail.com>
2021-05-10 22:38:58 +0300

    upload an image file

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -6,25 +6,39 @@ import PostProfile from './PostProfile' ;
 import Avatar from '@material-ui/core/Avatar' ;
 import Button from '@material-ui/core/Button' ;
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown' ;
-
+import Fade from 'react-reveal/Fade'
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 const Account = ({ setHidden }) => {
   const [ postFocus , setPostFocus ] = useState(null) ;
+  const [ file , setFile ] = useState() ;
   const user = useSelector(state => state.user) ;
   const dark = useSelector(state => state.darkTheme) ;
   const newUser = () => {
       setPostFocus(null) ;
       setHidden(false) ;
   }
+  const handleChange = (e) => {
+    setFile(URL.createObjectURL(e.target.files[0]))
+  }
   return (
     <div className='user-profile' style={{ background : dark ?  '#171717' : 'inherit' , color: dark ? 'white' : 'inherit'}}>
 
         <div className='user-profile-top'>
             <PostProfile newUser={newUser} postFocus={postFocus} setHidden={setHidden} setPostFocus={setPostFocus} comments={null} />
-            <Avatar className='user-profile-image' src={user.photoURL} alt={`${user.displayName} picture`}  />
+            <Avatar className='user-profile-image' src={file || user.photoURL} alt={`${user.displayName} picture`}  />
+
               <div className='user-top-info'>
                     <h1 style={{color: dark ? 'white' : '#535353'}}>{user.displayName}</h1>
                     <div>
+                    <input type="file" onChange={(e) => handleChange(e) } className='account-input' />
+                    <Button
+                      variant="contained"
+                      color="default"
+                      startIcon={<PhotoCamera />}
+                    >
+                      Upload
+                    </Button>
                         <Button >
                             <ArrowDropDownIcon />
                         </Button>
